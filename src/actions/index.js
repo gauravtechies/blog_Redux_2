24df2c074def5000354d4fa2876eb486e2f7243a
gauravtechies <gauravkamboj13121992@gmail.com>
2020-06-22 14:19:08 +0530

    redux-thunk implemented

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,14 @@
 import jsonPlaceholder from '../apis/jsonPLaceholder'
 
-export const  fetchPosts =async () =>{
-    const response= await jsonPlaceholder.get('/posts');
-    return {
-        type:'FETCH_POSTS',
-        payload: response
+// export const  fetchPosts =async () =>{
+//     return async function(dispatch,getState){
+//         const response= await jsonPlaceholder.get('/posts');
+//         dispatch({ type:'FETCH_POSTS',payload:response })
+//     }
+// }
+
+// we can short upper like
+export const  fetchPosts =async () => async dispatch=>{
+        const response= await jsonPlaceholder.get('/posts');
+        dispatch({ type:'FETCH_POSTS',payload:response })
     }
-}
